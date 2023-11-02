3a8bd9f0870729771fbc2be20269f6c0a19087a1
jisu <comjisu0311@naver.com>
2023-11-02 15:16:47 +0900

    feat: 계정생성시 MainActivity로 이동할 수 있는 버튼 기능 임시구현 #8 이동하는것 확인만하고 조건 추가적으로 구현해야함

--- app/src/main/java/com/example/podomarket/AccountCreateActivity.kt
+++ app/src/main/java/com/example/podomarket/AccountCreateActivity.kt
@@ -3,6 +3,7 @@ package com.example.podomarket
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.ktx.auth
@@ -15,11 +16,16 @@ class AccountCreateActivity: AppCompatActivity() {
         setContentView(R.layout.activity_account)
 
         //뒤로가기 버튼 구현(Manifest에 부모 액티비티를 LoginActivity로 설정)
-
         val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
+        findViewById<Button>(R.id.create_account_button)?.setOnClickListener {
+            //아이디, 비번, 이름, 날짜 로직 정확한지 비교해야함 성공하면 계정생성 성공, 아닐시에는 재입력 요구창 뜨게하기
+            startActivity( //지금 Main으로 이동안하는것처럼 보여도 Main이동하고 조건을 만족못해서 LoginActivity로 간다. 로직은 맞으니 추후 계정생성 성공으로만 바꾸면 MainActivity맞게뜰거임
+                Intent(this, MainActivity::class.java))
+            finish()
+        }
         /*
         val userEmail = findViewById<EditText>(R.id.username)?.text.toString()
         val password = findViewById<EditText>(R.id.password)?.text.toString()
