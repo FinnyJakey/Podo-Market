207f0baef97c1cffc97af8a39dcb3834e645dbeb
kgunwoo26 <56541313+kgunwoo26@users.noreply.github.com>
2023-11-09 15:04:05 +0900

    Feature/#18 레이아웃 수정 (#19)
    * feat: 메인 액티비티 시작 화면 변경, 필터링 체크버튼 추가, 로그아웃 버튼 추가
    
    * delete /.idea
    
    * Update .gitignore

--- app/src/main/java/com/example/podomarket/MainActivity.kt
+++ app/src/main/java/com/example/podomarket/MainActivity.kt
@@ -7,29 +7,31 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import com.example.podomarket.login.LoginActivity
+import com.example.podomarket.product.ProductListFragment
 import com.example.podomarket.viewmodel.AuthViewModel
 
 class MainActivity : AppCompatActivity() {
     private val authViewModel = AuthViewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_market)
 
         //로그인 한 상태로 실행되도록 하는 코드(바로 MainActivity갈경우만 필요) - 추후지워야함
-        doTestLogin()
+        //doTestLogin()
 
         if(authViewModel.getCurrentUserUid() == null){
             //로그인 안되어있을시 LoginActivity로 시작
             moveLoginActivity()
         }
-
-        findViewById<TextView>(R.id.textUID)?.text = authViewModel.getCurrentUserUid()?: "No User"
-
-        findViewById<Button>(R.id.button_signout)?.setOnClickListener {
+/*        findViewById<Button>(R.id.button_signout)?.setOnClickListener {
             //signout 버튼 클릭시 로그아웃+LoginActivity로 이동
             authViewModel.signOut()
             moveLoginActivity()
-        }
+        }*/
+        val productListFragment = ProductListFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, productListFragment)
+            .commit()
     }
 
     //바로 로그인 원할시 사용
@@ -41,6 +43,12 @@ class MainActivity : AppCompatActivity() {
             else Toast.makeText(this, "로그인 정보가 옳지 않습니다", Toast.LENGTH_SHORT).show()
         }
     }
+
+    fun Logout() {
+        authViewModel.signOut()
+        moveLoginActivity()
+    }
+
     private fun moveLoginActivity(){
         startActivity(Intent(this, LoginActivity::class.java))
         finish()
