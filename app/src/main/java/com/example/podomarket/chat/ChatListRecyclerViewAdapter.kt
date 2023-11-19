236dc159149133682c3f649ed5231cde55b9ea61
jisu <comjisu0311@naver.com>
2023-11-19 12:57:57 +0900

    feat: 채팅 리스트 기능 구현 #32
    더미데이터로 테스트 완료, 아직 ViewModel이랑 ChatRoom이랑은 테스트 해보진않음

--- app/src/main/java/com/example/podomarket/chat/ChatListRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/podomarket/chat/ChatListRecyclerViewAdapter.kt
@@ -4,11 +4,16 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.podomarket.R
+import com.example.podomarket.model.ChatRoomModel
+import com.example.podomarket.viewmodel.AuthViewModel
+import com.google.firebase.Timestamp
 
-class ChatListRecyclerViewAdapter(private val dataList: List<String>, private val itemClickListener: OnItemClickListener) : RecyclerView.Adapter<ChatListRecyclerViewAdapter.MyViewHolder>() {
+class ChatListRecyclerViewAdapter(private val dataList: List<ChatRoomModel>, private val itemClickListener: OnItemClickListener) : RecyclerView.Adapter<ChatListRecyclerViewAdapter.MyViewHolder>() {
+    private var authviewModel = AuthViewModel()
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val textView: TextView = itemView.findViewById(R.id.username)
-
+        val userNameText: TextView = itemView.findViewById(R.id.chat_list_username)
+        val messageTimeText: TextView = itemView.findViewById(R.id.chat_list_message_time)
+        val messageText: TextView = itemView.findViewById(R.id.chat_list_message_text)
         init {
             itemView.setOnClickListener {
                 val position = adapterPosition
@@ -23,13 +28,38 @@ class ChatListRecyclerViewAdapter(private val dataList: List<String>, private va
         return MyViewHolder(itemView)
     }
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val item = dataList[position]
-        holder.textView.text = item
+        val chatRoom : ChatRoomModel = dataList[position]
+        var currentUserId = authviewModel.getCurrentUserUid()
+        var anotherUserId = chatRoom.participants[0]
+        if(currentUserId == chatRoom.participants[0]) anotherUserId = chatRoom.participants[1]
+
+        authviewModel.getUserName(anotherUserId) { name ->
+            if (name != null) {
+                //대입
+                holder.userNameText.text = name
+                holder.messageTimeText.text = calculateTimeAgo(chatRoom.recentTime)
+                holder.messageText.text = chatRoom.chats[chatRoom.chats.size - 1].message
+            } else {
+                println("가져오기 실패")
+            }
+        }
     }
     override fun getItemCount(): Int {
         return dataList.size
     }
     interface OnItemClickListener {
         fun onItemClick(position: Int)
+    }
+    private fun calculateTimeAgo(createdAt: Timestamp): String {
+        val currentTime = Timestamp.now()
+        val timeDifferenceMillis = currentTime.toDate().time - createdAt.toDate().time
+
+        val minutes = (timeDifferenceMillis / (1000 * 60)).toInt()
+
+        return when {
+            minutes < 60 -> "${minutes}분 전"
+            minutes < 24 * 60 -> "${minutes / 60}시간 전"
+            else -> "${minutes / (24 * 60)}일 전"
+        }
     }
 }
